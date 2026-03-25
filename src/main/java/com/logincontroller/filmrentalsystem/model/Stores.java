64a15c04a4cad35a040d4ea34d91a02f7f0517b2
DivyanshPoddar31 <dpoddar3108@gmail.com>
2026-03-25 10:28:16 +0530

    Second commit

--- src/main/java/com/logincontroller/filmrentalsystem/model/Stores.java
+++ src/main/java/com/logincontroller/filmrentalsystem/model/Stores.java
@@ -1,4 +1,29 @@
 package com.logincontroller.filmrentalsystem.model;
 
+import jakarta.persistence.*;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.AllArgsConstructor;
+import java.time.LocalDateTime;
+
+@Data
+@NoArgsConstructor // Required by JPA
+@AllArgsConstructor
+@Entity
+@Table(name = "store")
 public class Stores {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "store_id")
+    private Integer storeId;
+
+    @Column(name = "manager_staff_id")
+    private Integer managerStaffId;
+
+    @Column(name = "address_id")
+    private Integer addressId;
+
+    @Column(name = "last_update")
+    private LocalDateTime lastUpdate;
 }
