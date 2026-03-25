e338548900abbce885e3ba7efaec6501220460fd
Debapriya <debapriya1203@gmail.com>
2026-03-25 11:36:15 +0530

    Initial commit

--- src/main/java/com/logincontroller/filmrentalsystem/model/Customer.java
+++ src/main/java/com/logincontroller/filmrentalsystem/model/Customer.java
@@ -1,4 +1,47 @@
 package com.logincontroller.filmrentalsystem.model;
 
+import java.time.LocalDateTime;
+
+
+import jakarta.persistence.*;
+        import lombok.*;
+
+        import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "customer")
+
+@Data               // getters + setters + toString
+@NoArgsConstructor  // default constructor
+@AllArgsConstructor // full constructor
+@Builder            // builder pattern
+
 public class Customer {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "customer_id")
+    private int customerId;
+
+    @Column(name = "store_id")
+    private int storeId;
+
+    @Column(name = "first_name")
+    private String firstName;
+
+    @Column(name = "last_name")
+    private String lastName;
+
+    private String email;
+
+    @Column(name = "address_id")
+    private int addressId;
+
+    private int active;
+
+    @Column(name = "create_date")
+    private LocalDateTime createDate;
+
+    @Column(name = "last_update")
+    private LocalDateTime lastUpdate;
 }
--- src/main/java/com/logincontroller/filmrentalsystem/service/RentalService.java
+++ src/main/java/com/logincontroller/filmrentalsystem/service/RentalService.java
@@ -1,4 +1,37 @@
 package com.logincontroller.filmrentalsystem.service;
 
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+import com.logincontroller.filmrentalsystem.model.Rental;
+import com.logincontroller.filmrentalsystem.repository.RentalRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
+
 public class RentalService {
+
+    private final RentalRepository rentalRepository;
+
+    public Rental save(Rental rental) {
+        return rentalRepository.save(rental);
+    }
+
+    public List<Rental> getAll() {
+        return rentalRepository.findAll();
+    }
+
+    public Rental getById(int id) {
+        return rentalRepository.findById(id).orElse(null);
+    }
+
+    public void delete(int id) {
+        rentalRepository.deleteById(id);
+    }
 }
