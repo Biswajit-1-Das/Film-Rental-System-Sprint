750f71fb9fde3f40ee72b140ece469d46ab28b75
Deblinadey18 <deblina.06.18@gmail.com>
2026-03-26 13:04:52 +0530

    added mappings in city and address

--- src/main/java/com/logincontroller/filmrentalsystem/model/City.java
+++ src/main/java/com/logincontroller/filmrentalsystem/model/City.java
@@ -1,68 +1,31 @@
 package com.logincontroller.filmrentalsystem.model;
 
+import lombok.*;
 import jakarta.persistence.*;
 
 import java.sql.Timestamp;
 
 @Entity
 @Table(name = "city")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class City {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "city_id")
-    private int cityId;
+    private Integer cityId;
 
     @Column(name = "city", nullable = false)
     private String city;
 
-    @Column(name = "country_id", nullable = false)
-    private int countryId;
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "country_id", nullable = false)
+    private Country country;
 
     @Column(name = "last_update", nullable = false)
     private Timestamp lastUpdate;
 
-    // Constructors
-    public City() {
-    }
 
-    public City(int cityId, String city, int countryId, Timestamp lastUpdate) {
-        this.cityId = cityId;
-        this.city = city;
-        this.countryId = countryId;
-        this.lastUpdate = lastUpdate;
-    }
-
-    // Getters and Setters
-    public int getCityId() {
-        return cityId;
-    }
-
-    public void setCityId(int cityId) {
-        this.cityId = cityId;
-    }
-
-    public String getCity() {
-        return city;
-    }
-
-    public void setCity(String city) {
-        this.city = city;
-    }
-
-    public int getCountryId() {
-        return countryId;
-    }
-
-    public void setCountryId(int countryId) {
-        this.countryId = countryId;
-    }
-
-    public Timestamp getLastUpdate() {
-        return lastUpdate;
-    }
-
-    public void setLastUpdate(Timestamp lastUpdate) {
-        this.lastUpdate = lastUpdate;
-    }
 }
