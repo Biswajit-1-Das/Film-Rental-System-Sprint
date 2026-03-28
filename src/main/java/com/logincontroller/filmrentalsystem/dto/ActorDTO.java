61892f9853b5a338aeb09db9e8670d6f13343f95
DivyanshPoddar31 <dpoddar3108@gmail.com>
2026-03-28 11:26:17 +0530

    added dto for actor
    # Conflicts:
    #	src/main/java/com/logincontroller/filmrentalsystem/controller/ActorController.java
    #	src/main/java/com/logincontroller/filmrentalsystem/dto/ActorDTO.java

--- src/main/java/com/logincontroller/filmrentalsystem/dto/ActorDTO.java
+++ src/main/java/com/logincontroller/filmrentalsystem/dto/ActorDTO.java
@@ -1,15 +1,21 @@
 package com.logincontroller.filmrentalsystem.dto;
 
+import com.logincontroller.filmrentalsystem.model.Actor;
 import lombok.Data;
-
-import java.time.LocalDateTime;
-import java.util.List;
+import java.sql.Timestamp;
 
 @Data
 public class ActorDTO {
     private Short actorId;
     private String firstName;
     private String lastName;
-    private LocalDateTime lastUpdate;
-    private List<String> filmTitles;
+    private Timestamp lastUpdate;
+
+    // This constructor does all the heavy lifting for you!
+    public ActorDTO(Actor actor) {
+        this.actorId = actor.getActorId();
+        this.firstName = actor.getFirstName();
+        this.lastName = actor.getLastName();
+        this.lastUpdate = actor.getLastUpdate();
+    }
 }
