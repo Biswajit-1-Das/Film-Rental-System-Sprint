dad4eea879eceb21106552197e50e558ab53c78f
Rajarshi07OK <karmakar1raj@gmail.com>
2026-03-29 16:59:12 +0530

    Actor DTO changed

--- src/main/java/com/logincontroller/filmrentalsystem/dto/ActorDTO.java
+++ src/main/java/com/logincontroller/filmrentalsystem/dto/ActorDTO.java
@@ -3,13 +3,15 @@ package com.logincontroller.filmrentalsystem.dto;
 import com.logincontroller.filmrentalsystem.model.Actor;
 import lombok.Data;
 import java.sql.Timestamp;
+import java.time.LocalDateTime;
+import java.time.LocalTime;
 
 @Data
 public class ActorDTO {
     private Short actorId;
     private String firstName;
     private String lastName;
-    private Timestamp lastUpdate;
+    private LocalDateTime lastUpdate;
 
     // This constructor does all the heavy lifting for you!
     public ActorDTO(Actor actor) {
