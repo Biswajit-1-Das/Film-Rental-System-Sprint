00607258aa11796576db5c8d788553721d96b874
Debapriya <debapriya1203@gmail.com>
2026-04-02 03:29:59 +0530

    modified missing annotations

--- src/main/java/com/logincontroller/filmrentalsystem/model/Film.java
+++ src/main/java/com/logincontroller/filmrentalsystem/model/Film.java
@@ -11,6 +11,8 @@ import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
 
+
+
 @Entity
 @Table(name = "film")
 @JsonIgnoreProperties({"hibernateLazyInitializer", "handler"})
